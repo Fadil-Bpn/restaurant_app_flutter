4bd9a0cf458a4187103f92fe2392e9ea843d2a89
Fadil-Bpn <rojafadilah0203@gmail.com>
2025-09-03 12:39:52 +0800

    Fixing theme.dart

--- lib/style/theme/theme.dart
+++ lib/style/theme/theme.dart
@@ -2,26 +2,23 @@ import 'package:flutter/material.dart';
 import '../color/color.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  // Default theme
+
   bool _isDarkMode = false;
   MaterialColor _primaryColor = AppColor.green;
 
   bool get isDarkMode => _isDarkMode;
   MaterialColor get primaryColor => _primaryColor;
 
-  // Toggle light/dark
   void toggleTheme() {
     _isDarkMode = !_isDarkMode;
     notifyListeners();
   }
 
-  // Ganti warna primer
   void setPrimaryColor(MaterialColor color) {
     _primaryColor = color;
     notifyListeners();
   }
 
-  // Dapatkan ThemeData sesuai kondisi
   ThemeData get themeData {
     return _isDarkMode
         ? ThemeData.dark().copyWith(
